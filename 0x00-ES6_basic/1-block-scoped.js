a47e15e20b830f93052fa68866fef5ff516f9e5e
Ousmane Konate <konatealim@gmail.com>
2023-03-22 22:23:26 +0100

    fixing style

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
+    /*eslint-disable */
     const task = true;
     const task2 = false;
   }
